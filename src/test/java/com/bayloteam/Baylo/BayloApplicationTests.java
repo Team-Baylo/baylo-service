de1258d371cdbc8b1abf9f9cc7ba67c47fe32fb7
Nick Faelnar <nickfaelnar@gmail.com>
2020-08-21 05:36:04 +0800

    Initial Commit

---
+++ src/test/java/com/bayloteam/Baylo/BayloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayloteam.Baylo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
